fde84e59a510966c0f6d10cde0180903217e0c4a
FiodorG <gre_dorelus@yahoo.com>
2021-12-17 12:37:37 -0500

    Assertion Introspect

--- test/test_math.py
+++ test/test_math.py
@@ -1,7 +1,19 @@
 """
-This module contains
+This module contains math operation for testing.
+Their purpose is to show how to use the pytest framework by example.
 """
-
+#----------------------------------------------------------------------------------------
+# A most basic function
+#----------------------------------------------------------------------------------------
 def test_one_plus_one():
     assert 1+1 == 2
     
+#----------------------------------------------------------------------------------------
+# A test function to show assertion introspection
+#----------------------------------------------------------------------------------------
+def test_one_plus_two():
+    a = 1
+    b = 2
+    c = 3
+    assert a + b == c
+
